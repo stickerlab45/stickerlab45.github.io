d79acdb2a97893d77960b5054640da1b6fff67b6
stickerlab45 <stickerlab45@gmail.com>
2026-08-13 21:10:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,18 +5,6 @@ all = document.querySelector("#all");
 
 let limit = 8;
 
-const images = {
-  "Jalur Evakuasi Kiri": "images/jalurevakuasikiri.jpg",
-  "Jalur Evakuasi Bawah": "images/jalurevakuasibawah.jpg",
-  "EXIT": "images/exit.jpg",
-  "FIRE EXIT": "images/fire exit.jpg",
-  "EXIT Kanan": "images/exit kanan.jpg",
-  "EXIT Kiri": "images/exit kiri.jpg",
-  "EXIT Atas": "images/exit atas atas kanan.jpg",
-  "EXIT Bawah": "images/exit bawah kanan.jpg",
-  "Titik Kumpul": "images/titikkumpul.jpg"
-};
-
 function render() {
   let q = search.value.toLowerCase();
   let c = cat.value;
@@ -26,31 +14,28 @@ function render() {
     (c === "Semua" || p[1] === c)
   );
 
-  grid.innerHTML = list.slice(0, limit).map(p => {
-    const image = images[p[0]];
+  grid.innerHTML = list.slice(0, limit).map(p => `
+    <article class="card">
 
-    return `
-      <article class="card">
-        <div class="signbox">
-          ${
-            image
-              ? `<img src="${image}" alt="${p[0]}" style="width:100%;height:100%;object-fit:contain;">`
-              : `<div class="sign ${p[3]}">${p[2]}</div>`
-          }
-        </div>
+      <div class="signbox">
+        ${p[4]
+          ? `<img src="${p[4]}" alt="${p[0]}" class="product-image">`
+          : `<div class="sign ${p[3]}">${p[2]}</div>`
+        }
+      </div>
 
-        <h3>${p[0]}</h3>
+      <h3>${p[0]}</h3>
 
-        <p>Tanda keselamatan – ukuran dan material dapat disesuaikan.</p>
+      <p>Tanda keselamatan – ukuran dan material dapat disesuaikan.</p>
 
-        <div class="actions">
-          <a href="https://wa.me/6282321000304">◉</a>
-          <a href="#">▣</a>
-          <a href="#">◉</a>
-        </div>
-      </article>
-    `;
-  }).join("");
+      <div class="actions">
+        <a href="https://wa.me/6285640971323">◉</a>
+        <a href="https://id.shp.ee/pbzR8VpF">▣</a>
+        <a href="https://vt.tiktok.com/ZSV1DRA7N/?page=Mall">◉</a>
+      </div>
+
+    </article>
+  `).join("");
 
   all.style.display = list.length > limit ? "block" : "none";
 }
